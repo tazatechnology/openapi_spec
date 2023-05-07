d86af9583fceee4f268f9d025eb455c8d662839c
Wahab Alshahin <wahab.alshahin@gmail.com>
2023-05-07 02:40:17 -0400

    Initial commit

---
+++ example/openapi_spec_example.dart
@@ -0,0 +1,6 @@
+import 'package:openapi_spec/openapi_spec.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/openapi_spec.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library openapi_spec;
+
+export 'src/openapi_spec_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
