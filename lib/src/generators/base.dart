1399e9e670c2729c3b62d55a3f14adb4a4827cb7
David Miguel <me@davidmiguel.com>
2023-10-10 08:23:42 +0200

    Fix static analysis warning in generated files

--- lib/src/generators/base.dart
+++ lib/src/generators/base.dart
@@ -38,6 +38,7 @@ abstract class BaseGenerator {
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
+// ignore_for_file: invalid_annotation_target
 """;
     return header;
   }
