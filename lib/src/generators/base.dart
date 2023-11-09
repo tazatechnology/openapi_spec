a7204d79931c9ba99f889e20d7c8e620f8457340
David Miguel <me@davidmiguel.com>
2023-11-09 18:18:55 +0100

    Ignore unused import warning in generated files

--- lib/src/generators/base.dart
+++ lib/src/generators/base.dart
@@ -38,7 +38,7 @@ abstract class BaseGenerator {
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: invalid_annotation_target
+// ignore_for_file: invalid_annotation_target, unused_import
 """;
     return header;
   }
